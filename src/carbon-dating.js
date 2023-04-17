8cc5f64a54e9e30a62270445d54661b5fd81dce5
alexsteed3228 <114911842+alexsteed3228@users.noreply.github.com>
2023-04-17 22:48:02 +0300

    feat: add func

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,12 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if(!(typeof sampleActivity == 'string') ||  +sampleActivity > 15 || !(+sampleActivity) || +sampleActivity <0){
+    return false
+  }
+  return Math.ceil((Math.log(MODERN_ACTIVITY/(+sampleActivity))*HALF_LIFE_PERIOD)/Math.log(2));
+  
 }
 
 module.exports = {
