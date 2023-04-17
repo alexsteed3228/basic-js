3847f96acaff846f6a91c07e26ec4098911060f8
alexsteed3228 <114911842+alexsteed3228@users.noreply.github.com>
2023-04-17 22:53:00 +0300

    feat: add func

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam( members ) {
+  let res = [];
+  if(!Array.isArray(members)){
+    return false;
+  }
+  for(let i = 0; i<members.length; i++){
+    if(typeof members[i] == 'string' ){
+      members[i] = members[i].trim();
+      for(let j = 0; j<members[i].length; j++){
+        if (members[i][j] !== ' '){
+          res.push(members[i][0].toUpperCase());
+          break
+        }
+      }
+      
+    }
+  }
+  return res.sort().join('');
+  
 }
 
 module.exports = {
